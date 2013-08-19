10b7176f0833fa0ba4cc82677c895985b8dab01d
Gabriel Lafontant <glafonta@gmail.com>
2013-08-19 12:21:40 -0400

    Finally figured out the input validation prompt

--- guess_the_number/game.rb
+++ guess_the_number/game.rb
@@ -8,15 +8,16 @@ while true
   puts correct_answer
   perfect_response = response.to_i
 
-  if response == response.to_s
-    puts "Invalid inputs, must enter a number between 0 and #{max}."
-  end
- 
+
   case 
+  when response.to_i.to_s != response
+    puts "Invalid, input must enter a number between 0 and #{max}."
   when perfect_response > correct_answer
     puts "Too high, try again."
   when perfect_response < correct_answer
     puts "Too low, try again."
+  when response == response.to_s
+    puts "Invalid input, must enter a number between 0 and #{max}."
   else 
     if perfect_response == correct_answer
     puts "Congratulations, you guessed the number!"
