a0ced6464e4b0fb1827571956d25e30f6718880f
Gabriel Lafontant <glafonta@gmail.com>
2013-08-18 21:09:28 -0400

    added random feature to file

--- guess_the_number/game.rb
+++ guess_the_number/game.rb
@@ -3,7 +3,7 @@ max = 1000
 while true
   print "Guess a number between 0 and #{max}: " 
   response = gets.chomp
-  correct_answer = 275
+  correct_answer = rand(0..1000)
   perfect_response = response.to_i
 
   case 
@@ -11,12 +11,10 @@ while true
     puts "Too high, try again."
   when perfect_response < correct_answer
     puts "Too low, try again."
-  when perfect_response == correct_answer
+  else 
+    if perfect_response == correct_answer
     puts "Congratulations, you guessed the number!"
       break
-  else 
-    if response == response.to_s
-    puts "Invalid input, must enter a number between 0 and #{max}."
     end
   end
 end
