6b8feb299da3897281d6145355ee208bc80f4bef
Gabriel Lafontant <glafonta@gmail.com>
2013-08-19 10:27:45 -0400

    removed correct answer variable from loop

--- guess_the_number/game.rb
+++ guess_the_number/game.rb
@@ -1,15 +1,13 @@
 max = 1000
+correct_answer = rand(0..1000)  
 
 while true
   print "Guess a number between 0 and #{max}: " 
   response = gets.chomp
-  correct_answer = rand(0..1000)
+
+  puts correct_answer
   perfect_response = response.to_i
-
-  if response == response.to_s
-    puts "Invalid input, must enter a number between 0 and #{max}."
-  end
-
+ 
   case 
   when perfect_response > correct_answer
     puts "Too high, try again."
